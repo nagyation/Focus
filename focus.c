19e6433e29cfa6bfdd29dde745ac29009af4db1e
Mahmoud Nagy <mnagy1312@gmail.com>
2018-04-12 03:10:25 +0200

    init service

--- focus.c
+++ focus.c
@@ -20,49 +20,6 @@ unsigned int period = 10 * MINS;
 unsigned int duration = 0;
 char *body= "Focus!";
 char *title = "Remember!";
-int period_counter =0;
-pthread_t timer;
-
-static void daemonize(void)
-{
-  pid_t pid, sid;
-
-  /* already a daemon */
-  if ( getppid() == 1 ) return;
-
-  /* Fork off the parent process */
-  pid = fork();
-  if (pid < 0) {
-    exit(EXIT_FAILURE);
-  }
-  /* If we got a good PID, then we can exit the parent process. */
-  if (pid > 0) {
-    exit(EXIT_SUCCESS);
-  }
-
-  /* At this point we are executing as the child process */
-
-  /* Change the file mode mask */
-  umask(0);
-
-  /* Create a new SID for the child process */
-  sid = setsid();
-  if (sid < 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  /* Change the current working directory.  This prevents the current
-     directory from being locked; hence not being able to remove it. */
-  if ((chdir("/")) < 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  /* Redirect standard files to /dev/null */
-  freopen( "/dev/null", "r", stdin);
-  freopen( "/dev/null", "w", stdout);
-  freopen( "/dev/null", "w", stderr);
-
-}
 
 void help_screen(){
   printf("The arguments available are:\n");
@@ -77,6 +34,7 @@ void help_screen(){
 void close_program(int signal){
   show_notification ("I'm Closing - Focus", "Hope you've done well");
   uninit_notification();
+  uninit_notification_handler();
   exit(signal);
 }
 
@@ -112,7 +70,6 @@ void system_tray_callback(int action)
 int main(int argc,char *argv[]) {
   fflush(stdout); // clearing the buffer
 
-  printf("This is A daeomon process\n");
   if (argc < 2) // checking if no arguments sent
     {
       printf("This is the default mode\n");
@@ -120,7 +77,7 @@ int main(int argc,char *argv[]) {
     }
   
     
-  int i;
+  int i,service =0;
 	
   //checking for argumets
   for(i = 1;i < argc ; i++)
@@ -138,31 +95,39 @@ int main(int argc,char *argv[]) {
           help_screen();
           exit(EXIT_SUCCESS);
         }
+      else if(CMP_ARG(argv[i],"-s"))
+      {
+	  service = 1;
+      }
     }
 
-  if(!duration) // if it's zero make it infinity
-    duration = INF;
+  
 
-  daemonize();
-
-  //recieve Terminate signals from kill
-  signal(SIGINT, signal_callback);
-  signal(SIGTERM, signal_callback);
+  if(service)
+  {
+      //recieve Terminate signals from kill
+      signal(SIGINT, signal_callback);
+      signal(SIGTERM, signal_callback);
     
 
-  //Intializing gtk
-  gtk_init(&argc, &argv);
-  gtk_window_set_default_icon_name (APP_LOGO);
-    
-  init_notification();
-  init_notification_handler();
-  struct notification_data *d = new_notification_data(title,body,period,duration);
+      //Intializing gtk
+      gtk_init(&argc, &argv);
+      gtk_window_set_default_icon_name (APP_LOGO);
 
-  add_new_notification(d);
+  
+      intiate_tray_icon(title,system_tray_callback);
 
-  intiate_tray_icon(title,system_tray_callback);
-    
-  gtk_main();
-  uninit_notification();
-  uninit_notification_handler();
+      
+      init_notification();
+      init_notification_handler();
+      gtk_main();
+  }
+  else
+  {
+      if(!duration) // if it's zero make it infinity
+	  duration = INF;
+      struct notification_data *d = new_notification_data(title,body,period,duration);
+      
+      add_new_notification(d);
+  }
 }
